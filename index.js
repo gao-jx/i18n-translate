beff1331d25f912b739152e31c7ebf8a9c2bb9ed
gao-jx <azjgjx19911205@gmail.com>
2018-10-30 18:06:31 +0800

    支持繁体翻译

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
-module.exports = function (content) {
-  const value = JSON.stringify(content)
-  const exportData = `module.exports = function (Component) {
-    Component.options.__doc = ${value}
-  }`
-  this.callback(null, exportData)
-  // if (this.version && this.version >= 2) {
-  //   try {
-  //     this.cacheable && this.cacheable()
-  //     this.callback(null, `module.exports = ${generateCode(content)}`)
-  //   } catch (err) {
-  //     this.emitError(err.message)
-  //     this.callback(err)
-  //   }
-  // } else {
-  //   const message = 'support webpack 2 later'
-  //   this.emitError(message)
-  //   this.callback(new Error(message))
-  // }
-  // return `module.exports = ${content}`
+const http = require("http")
+const token = '98251bf17d0c634d09f90d204e2304d1'
+const qs = require('querystring')
+
+const uploadData = async (postData) => {
+  let chunk = ''
+  const option = {
+    hostname: '172.28.48.19',
+    port: 4041,
+    path: `/opencc`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }
+  return new Promise((resolve, reject) => {
+    let req = http.request(option, (res) => {
+      res.on('data', d => {
+        chunk += d
+      })
+
+      res.on('end', d => {
+        try {
+          resolve(JSON.parse(chunk))
+        } catch (err) {
+          throw err
+        }
+      })
+      res.on('error', d => {
+        throw d
+      })
+    })
+    req.write(postData)
+    req.end()
+  })
 }
-
-function generateCode (content) {
-  let code = ''
-
-  let value = typeof content === 'string'
-    ? JSON.parse(content)
-    : content
-  value = JSON.stringify(value)
-    .replace(/\u2028/g, '\\u2028')
-    .replace(/\u2029/g, '\\u2029')
-    .replace(/\\/g, '\\\\')
-
-  code += `function (Component) {
-  Component.options.__i18n = Component.options.__i18n || []
-  Component.options.__i18n.push('${value.replace(/\u0027/g, '\\u0027')}')
-  delete Component.options._Ctor
-}\n`
-  return code
+module.exports = async function (content) {
+  if (this.resource.indexOf('index.yaml') > -1) {
+    try {
+      const jsonData = content.replace('module.exports = ', '')
+        .replace(';', '')
+      const zh_cn = JSON.stringify(JSON.parse(jsonData).zh)
+      var postData = qs.stringify({
+        'token': token,
+        'from_lang': 'zh',
+        'to_lang': 'cht',
+        'query': zh_cn,
+        'type': 'community'
+      })
+      const tw = await uploadData(postData)
+      const i18nData = {
+        zh: JSON.parse(jsonData).zh,
+        tw: JSON.parse(tw.dst)
+      }
+      return `module.exports = ${JSON.stringify(i18nData)}`
+    } catch (err) {
+      return content
+    }
+  }
+  return content
 }
