bcc9c3e193857726df7576baffa4555d98f9f921
gao-jx <azjgjx19911205@gmail.com>
2019-01-02 18:55:50 +0800

    翻译<i18n>中的文件

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const Opencc = require('opencc')
 const instance = new Opencc("s2t.json")
+const utils = require('loader-utils')
 
-module.exports = async function (content) {
-  // find index.yaml
+module.exports = function (content) {
+  const userOptions = utils.getOptions(this)
+  if (userOptions && userOptions.type === 'i18n') {
+    try {
+      const zh = JSON.parse(content).zh
+      const i18nData = {
+        zh,
+        tw: JSON.parse(instance.convertSync(JSON.stringify(zh)))
+      }
+      return JSON.stringify(i18nData)
+    } catch (err) {
+      return content
+    }
+  }
+
   if (this.resource.indexOf('index.yaml') > -1) {
     try {
       const regSemicolon = /[\n.]*;$/g
